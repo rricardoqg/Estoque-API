8357728bd3d75ab317a3b064fed9769ba3f75929
Ricardo <redew326@gmail.com>
2026-06-11 15:31:50 -0300

    feat: migra para PostgreSQL com Docker

--- app/database.py
+++ app/database.py
@@ -1,16 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/estoque"
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./estoque.db")
-
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
